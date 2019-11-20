98f06b7689ba98e282c8af7ab7d8204cb12639cb
Hamza Alam <getright_02@live.com>
2019-11-20 18:47:19 +0500

    project creation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './header/header'
+import './App.css';
+
+function App() {
+  return (
+      <React.Fragment>
+        <Header/>
+        <div className="App">
+          <header className="App-header">
+          </header>
+        </div>
+      </React.Fragment>
+  );
+}
+
+export default App;
