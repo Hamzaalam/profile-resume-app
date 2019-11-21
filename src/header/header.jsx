ae1b51ecf85ed0d4c80728970b3355558125c038
Hamza Alam <getright_02@live.com>
2019-11-21 19:03:20 +0500

    nav-bar-tab created

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import red from '@material-ui/core/colors/red';
 import './header.css';
+
+
+
 const Header = () => {
-    return (<div className='header'>Header</div> );
+    const [value, setValue] = React.useState(2);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    const whiteRed = red[50]; 
+    return (<div className='header'>
+                    <Tabs
+                        className="nav-bar-tabs"
+                        value={value}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        onChange={handleChange}
+                        aria-label="disabled tabs example"
+                     >
+                        <Tab
+                            className="nav-bar-tabs"
+                            label="Me" 
+                        />
+                        <Tab                   
+                            className="nav-bar-tabs"
+                            label="Resume" 
+                        />
+                        <Tab
+                            className="nav-bar-tabs" 
+                            label="Projects" 
+                        />
+                        <Tab
+                            className="nav-bar-tabs" 
+                            label="Projects" 
+                        />
+                        <Tab 
+                            className="nav-bar-tabs" 
+                            label="Blog" 
+                        />
+                    </Tabs>
+            </div> );
 }
  
 export default Header
