d3a74380cac28a1498cfd0fb4da58e28d9d09667
Hamza Alam <smhamza124@gmail.com>
2019-12-03 01:38:59 +0500

    setup home page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Header from './header/header'
 import Main from './Main/Main'
 import './App.css';
-function App() {
+import { MuiThemeProvider, createMuiTheme , darken} from '@material-ui/core/styles'
+
+
+const font = "'Open Sans', sans-serif"; 
+const muiTheme = createMuiTheme({
+  fontFamily: font,
+}
+);
+
+function App(props) {
   return (
       <React.Fragment>
-        <Header/>
-        <div className="App">
-          <header className="App-header">
-            <Main/>  
-          </header>
-        </div>
+        <MuiThemeProvider theme={muiTheme}>
+          <div style={{fontFamily: font}}>
+            <Header/>
+            <div className="App">
+              <header className="App-header">
+                <Main/>  
+              </header>
+            </div>
+          </div>
+        </MuiThemeProvider>
       </React.Fragment>
   );
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import './Home.css'
 
 const Home = () => {
-    return ( <div><h2>Home</h2></div> );
+    return ( <div className="home">
+                <img 
+                     className="profile-image"
+                     src={process.env.PUBLIC_URL + '/0.jpeg'} 
+                     alt="profile-image"
+                />
+                <Typography variant="h4">
+                    Hello, I’m Hamza Alam!
+                </Typography>
+            </div> );
 }
  
 export default Home;
