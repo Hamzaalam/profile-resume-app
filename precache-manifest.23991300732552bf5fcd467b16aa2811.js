c4723df83b51d1b14668f31bb9df623cec4d51f0
Hamza Alam <smhamza124@gmail.com>
2019-12-09 23:02:40 +0500

    Updates

---
+++ precache-manifest.23991300732552bf5fcd467b16aa2811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819a58f75f339e2ea34674e4842b91da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aaa2782378a59d6c126",
+    "url": "/static/css/main.8810b4ac.chunk.css"
+  },
+  {
+    "revision": "68b713c43da98bec7738",
+    "url": "/static/js/2.28b76288.chunk.js"
+  },
+  {
+    "revision": "5aaa2782378a59d6c126",
+    "url": "/static/js/main.a85aab91.chunk.js"
+  },
+  {
+    "revision": "dd0a478bd4567a54c309",
+    "url": "/static/js/runtime-main.e427d7a5.js"
+  }
+]);
