63b95d0ab512264099fb51686f6e5429daa01802
smhamza124 <smhamza124@gmail.com>
2020-03-05 16:26:25 +0500

    THANK YOU

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -37,65 +37,104 @@ const Contact = (props) => {
         color:'#333',
 
     }
+    const thankyouImage = {
+        // borderRadius: '100%',
+        // width: 190,
+        display: 'flex',
+        margin: '0 auto',
+    }
+    const [message, setMessage] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [status, setStatus] = React.useState(false);
+
+
+    const handleContactForm =() =>{
+        console.log("Message ===> ", message);
+        console.log("email ===>  ", email);
+        setStatus(true)
+        
+    }
+
     const { classes } = props;
     return (
             <div style={content}>
                 <section style={section}>
+                {status? 
                     <Grid container>
                         <Grid item xs={12}>
-                            <Typography variant='h4' style={heading}> Hi there!</Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container style={{marginTop:'20px'}}>
-                        <Grid item xs={12}>
-                            <Typography classes={{ root: classes.Typography }}> 
-                                    Want to tell me something interesting? Have a suggestion for me? I'd love to hear from you!
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Grid container style={{margin:'20px 0px'}}>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                label="Email"
-                                style={{width:'300px'}}
-                                InputProps={{
-                                    classes: {
-                                      notchedOutline: classes.notchedOutline
-                                    }
-                                }}
+                            <Typography variant='h4' style={heading}> Thank you for contacting me!</Typography>
+                            <img
+                                style={thankyouImage}
+                                src={process.env.PUBLIC_URL + '/thankyou.gif'}
+                                alt="Thank you for contacting me!"
                             />
                         </Grid>
                     </Grid>
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="outlined-multiline-static"
-                                label="Message"
-                                multiline
-                                rows="6"
-                                variant="outlined"
-                                fullWidth
-                                InputProps={{
-                                    classes: {
-                                      notchedOutline: classes.notchedOutline
-                                    }
-                                  }}
-                            />
-                        </Grid>
+                : 
+                <>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Typography variant='h4' style={heading}> Hi there!</Typography>
                     </Grid>
-                    <Grid container style={{marginTop:'20px'}}>
-                        <Grid item xs={12}>
-                            <Button
-                                variant='outlined'
-                                size='large'
-                                color="primary"
-                                classes={{ root: classes.Button }}
-                            >
-                                Send Now
-                            </Button>
-                        </Grid>
+                </Grid>
+        
+                <Grid container style={{marginTop:'20px'}}>
+                    <Grid item xs={12}>
+                        <Typography classes={{ root: classes.Typography }}> 
+                                Want to tell me something interesting? Have a suggestion for me? I'd love to hear from you!
+                        </Typography>
                     </Grid>
+                </Grid>
+                    
+                <Grid container style={{margin:'20px 0px'}}>
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={(event)=>setEmail(event.target.value)}
+                            variant="outlined"
+                            label="Email"
+                            style={{width:'300px'}}
+                            InputProps={{
+                                classes: {
+                                notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <TextField
+                            onChange={(event)=>setMessage(event.target.value)}
+                            id="outlined-multiline-static"
+                            label="Message"
+                            multiline
+                            rows="6"
+                            variant="outlined"
+                            fullWidth
+                            InputProps={{
+                                classes: {
+                                notchedOutline: classes.notchedOutline
+                                }
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+                    
+                <Grid container style={{marginTop:'20px'}}>
+                    <Grid item xs={12}>
+                        <Button
+                            variant='outlined'
+                            size='large'
+                            color="primary"
+                            classes={{ root: classes.Button }}
+                            onClick={handleContactForm}
+                        >
+                            Send Now
+                        </Button>
+                    </Grid>
+                </Grid>
+                </>
+                }
                 </section>
             </div> 
            );
